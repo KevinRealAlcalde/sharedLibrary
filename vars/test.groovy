717c8a3a25aac2013b88b6c1ce5f8f1b3d340ab4
KevinRealAlcalde <95503519+KevinRealAlcalde@users.noreply.github.com>
2024-05-20 10:11:30 +0100

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,5 +1,7 @@
 def call () {
-  script {
+
+
+script {
     try {
         testFiles = ["1", "2", "3", "4"]
         def parallelStagesMap = [:]
@@ -10,5 +12,25 @@ def call () {
     } catch (Exception e) {
         echo 'Exception occurred with parallel stage: ' + e
     }
-  }
+}
+
+  def generateStage(podLabel, customWorkspace) {
+    return {
+        stage("Runs on ${podLabel}") {
+            podTemplate(
+            inheritFrom: 'cloud') {
+                node("test") { ws("${customWorkspace}") {
+                        try {
+                            stage("Before") {
+                                echo 'look we ran'
+                                sleep(60)
+                            }
+                        }catch(Exception e){
+                            echo "Error " + e
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
